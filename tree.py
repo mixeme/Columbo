b0ec8e3badc5ed54a7b9fed93efdbf8555752ba6
mixeme <mix.public@ya.ru>
2024-01-08 21:21:54 +0300

    Add class for rows comparison

--- tree.py
+++ tree.py
@@ -12,6 +12,39 @@ class TreeType(Enum):
     BYDATE = 1
 
 
+class FileSortFilterProxyModel(QtCore.QSortFilterProxyModel):
+    def lessThan(self, left, right):
+        # Retrieve names
+        left_name = left.data()
+        right_name = right.data()
+        # Retrieve dates
+        left_date = left.siblingAtColumn(1).data()
+        right_date = right.siblingAtColumn(1).data()
+        # Retrieve snapshots
+        left_snapshot = left.siblingAtColumn(2).data()
+        right_snapshot = right.siblingAtColumn(2).data()
+
+        # Left & right items are folders
+        if (left_date == "Folder") and (right_date == "Folder"):
+            return left_name < right_name   # Compare their names
+
+        # Left - folder, right - file
+        if left_date == "Folder":
+            return True                     # Folder less than file
+
+        # Right - folder, left - file
+        if right_date == "Folder":
+            return False                    # Folder not less than file
+
+        # Left & right items are files with the same name
+        # This is case "by date -> unified"
+        if left_name == right_name:
+            return left_snapshot < right_snapshot   # Compare timestamp of snapshots
+
+        # All other cases
+        return left_name < right_name
+
+
 class FileTreeWorker(QRunnable):
     columns = ["Name", "Last modified", "Snapshot"]
 
