ef7db4c7492944f9e465c22951118d817f5d459b
mixeme <mix.public@ya.ru>
2024-01-11 12:42:41 +0300

    Use singleton for icons

--- pkg/nodes.py
+++ pkg/nodes.py
@@ -3,11 +3,11 @@ import os
 from PyQt5.QtGui import QStandardItem
 from pkg import file, icons
 
-icon_loader = None
 
 TreeNode = list[QStandardItem]      # Type alias for return values
 
-def create_folder(path):
-    return [QStandardItem(icon_loader.folder, path),
+
+def create_folder(path: str) -> TreeNode:
+    return [QStandardItem(icons.IconsLoader.singleton.folder, path),
             QStandardItem("Folder"),
             QStandardItem("---")]
