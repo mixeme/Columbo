97203d699ce09cd681a1bd55276460d424b81b53
mixeme <mix.public@ya.ru>
2024-01-11 09:39:55 +0300

    Add icons loader class to the package

---
+++ pkg/icons.py
@@ -0,0 +1,8 @@
+from PyQt5.QtGui import QIcon
+
+
+class IconsLoader:
+    def __init__(self) -> None:
+        super().__init__()
+        self.folder = QIcon.fromTheme("folder", QIcon("icons/folder.png"))
+        self.file = QIcon.fromTheme("text-x-generic", QIcon("icons/file.png"))
