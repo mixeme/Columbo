c684256571a24ccb82e26cfd9c2959f62878310c
mixeme <mix.public@ya.ru>
2024-01-11 12:45:26 +0300

    Introduce singleton

--- pkg/icons.py
+++ pkg/icons.py
@@ -2,7 +2,10 @@ from PyQt5.QtGui import QIcon
 
 
 class IconsLoader:
+    singleton = None
+
     def __init__(self) -> None:
         super().__init__()
         self.folder = QIcon.fromTheme("folder", QIcon("icons/folder.png"))
         self.file = QIcon.fromTheme("text-x-generic", QIcon("icons/file.png"))
+        IconsLoader.singleton = self
