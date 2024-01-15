cc042756aae07955427ccc096f179e50b7e3b1f7
mixeme <mix.public@ya.ru>
2024-01-15 22:57:18 +0300

    Add environment

--- scripts/build-lnx.sh
+++ scripts/build-lnx.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env sh
 
-ARCH=$(uname -m)
+# Change location to project home
+cd $(dirname $0);
+cd ..;
+echo "Project home:" $PWD;
 
+# Get platform architecture
+ARCH=$(uname -m);
+echo "Detected platform architecture:" $ARCH
+
+# Create executable file
 python -m PyInstaller \
 	--clean \
 	--windowed \
@@ -10,4 +18,6 @@ python -m PyInstaller \
 	--add-data="./gui:./gui" \
 	--add-data="./icons:./icons" \
 	--icon=icons/search.ico \
-	main.py
+	main.py;
+
+exit 0;
