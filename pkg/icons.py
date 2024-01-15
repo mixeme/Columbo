463e549b1c019cce705aa748facfdd9a301c0288
mixeme <mix.public@ya.ru>
2024-01-15 22:18:46 +0300

    Add resolving of project home

--- pkg/icons.py
+++ pkg/icons.py
@@ -1,3 +1,6 @@
+from os import path
+import sys
+
 from PyQt5.QtGui import QIcon
 
 
@@ -6,6 +9,15 @@ class IconsLoader:
 
     def __init__(self) -> None:
         super().__init__()
-        self.folder = QIcon.fromTheme("folder", QIcon("icons/folder.png"))
-        self.file = QIcon.fromTheme("text-x-generic", QIcon("icons/file.png"))
+
+        # Resolve project home folder
+        if getattr(sys, 'frozen', False):
+            # we are running in a bundle
+            bundle_dir = sys._MEIPASS
+        else:
+            # we are running in a normal Python environment
+            bundle_dir = path.dirname(path.dirname(path.abspath(__file__)))
+
+        self.folder = QIcon.fromTheme("folder", QIcon(path.join(bundle_dir, "icons/folder.png")))
+        self.file = QIcon.fromTheme("text-x-generic", QIcon(path.join(bundle_dir, "icons/file.png")))
         IconsLoader.singleton = self
