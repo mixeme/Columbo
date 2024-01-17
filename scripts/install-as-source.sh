ff8f872f4f23abd04a30dbc7e91151db5ac05eab
mixeme <mix.public@ya.ru>
2024-01-17 21:47:05 +0300

    Add code

---
+++ scripts/install-as-source.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env sh
+
+# wget https://github.com/mixeme/Columbo/raw/dev/scripts/install-as-source.sh | sudo bash
+
+COMMAND=$1
+
+case $COMMAND in
+	source )
+	;;
+	binary )
+	;;
+	standalone )
+	;;
+	* )
+	echo "Unknown command" $COMMAND;
+	echo "Possible commands: source | binary | standalone";
+esac
+
+cd $(dirname $0)
+
+COLUMBO_HOME=/opt
+COLUMBO_PY=Columbo/main.py
+
+apt install git python3 python3-pip python3-pyqt5;
+cd $COLUMBO_HOME;
+git clone https://github.com/mixeme/Columbo.git;
+
+case $COMMAND in
+	source )
+		chmod +x $COLUMBO_PY;
+		ln -s $COLUMBO_HOME/$COLUMBO_PY /usr/local/bin/columbo;
+	;;
+	binary )
+		bash build-lnx.sh onedir
+	;;
+	standalone )
+		bash build-lnx.sh onefile
+	;;
+esac
+
