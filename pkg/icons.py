32db61216aa32da699cefd83bd62bfaced2c6ab6
mixeme <mix.public@ya.ru>
2024-01-17 22:01:59 +0300

    Use input argument instead

--- pkg/icons.py
+++ pkg/icons.py
@@ -7,17 +7,8 @@ from PyQt5.QtGui import QIcon
 class IconsLoader:
     singleton = None
 
-    def __init__(self) -> None:
+    def __init__(self, project_home) -> None:
         super().__init__()
-
-        # Resolve project home folder
-        if getattr(sys, 'frozen', False):
-            # we are running in a bundle
-            bundle_dir = sys._MEIPASS
-        else:
-            # we are running in a normal Python environment
-            bundle_dir = path.dirname(path.dirname(path.abspath(__file__)))
-
-        self.folder = QIcon.fromTheme("folder", QIcon(path.join(bundle_dir, "icons/folder.png")))
-        self.file = QIcon.fromTheme("text-x-generic", QIcon(path.join(bundle_dir, "icons/file.png")))
+        self.folder = QIcon.fromTheme("folder", QIcon(path.join(project_home, "icons/folder.png")))
+        self.file = QIcon.fromTheme("text-x-generic", QIcon(path.join(project_home, "icons/file.png")))
         IconsLoader.singleton = self
