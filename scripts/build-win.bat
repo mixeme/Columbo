02bba016e900229fe11a00f1e522872eecdc10d7
mixeme <mix.public@ya.ru>
2024-01-17 21:47:55 +0300

    Add command to setup work directory

--- scripts/build-win.bat
+++ scripts/build-win.bat
@@ -1,9 +1,28 @@
+:: Run from project home with
+:: .\scripts\build-win.bat
+
+:: Switch off output for each command
+@echo off
+
+:: Determine location of script
+SET SCRIPT=%~dp0
+echo Script's location: %SCRIPT%
+
+:: Go to project home
+cd %SCRIPT%
+cd ..
+echo Switch to project home: %cd%
+
+:: Run PyInstaller
 python -m PyInstaller ^
 	--clean ^
-	--windowed ^
-	--onefile ^
 	--name columbo-win ^
 	--add-data="./gui;./gui" ^
 	--add-data="./icons;./icons" ^
 	--icon=icons/search.ico ^
 	main.py
+
+:: 	--windowed ^
+::	--onefile ^
+
+exit 0
