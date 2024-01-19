2ccc833a2f253901782662a2a2f0c35f5ecf10d3
mixeme <mix.public@ya.ru>
2024-01-19 21:59:07 +0300

    Add messages

--- scripts/install.sh
+++ scripts/install.sh
@@ -34,21 +34,26 @@ COLUMBO_HOME=/opt;
 COLUMBO_PY="$COLUMBO_HOME/Columbo/main.py";
 COLUMBO_BIN="$COLUMBO_HOME/Columbo/columbo-$ARCH";
 
+echo "+ Install packages...";
 apt update -y && apt install -y git python3 python3-pip python3-pyqt5;
 pip install --upgrade pyinstaller
 
 # If `pip` command is unknown then
 # 	python -m pip install --upgrade pyinstaller
 
+echo "+ Clone Columbo repo...";
 cd "$COLUMBO_HOME";
 git clone -b $BRANCH https://github.com/mixeme/Columbo.git;
 
 case $COMMAND in
 	source )
+	  echo "+ Make main script executable...";
 		chmod +x $COLUMBO_PY;
+		echo "+ Make main script executable...";
 		ln -s $COLUMBO_PY /usr/local/bin/columbo;
 	;;
 	binary )
+	  echo "+ Build regular binary...";
 		bash build-lnx.sh onedir;
 		chmod +x "$COLUMBO_BIN";
 		ln -s "$COLUMBO_BIN" /usr/local/bin/columbo;
