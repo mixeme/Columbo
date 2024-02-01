3fbecef608e81720f9975ba0257c890e78024f84
mixeme <mix.public@ya.ru>
2024-02-02 00:06:29 +0300

    Update build-docker.sh

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -10,43 +10,106 @@ ARCH=$(uname -m);
 BIN_NAME="columbo-$ARCH";
 echo "Detected platform architecture: $ARCH";
 
-echo "Available images:
-[1] Debian 11 (reference)
-[2] Debian 10 (legacy)
-[3] CentOS 7  (legacy)
-"
-read -p "Select image. Enter number of an option: " ANS;
-
-IMAGE_BASENAME="mixeme/columbo";
-case $ANS in
+# Resolve a number of arguments
+case $# in
+	0 )
+	;;
 	1 )
-		IMAGE_NAME="$IMAGE_BASENAME:debian-bullseye-$ARCH";
-		DOCKERFILE=Dockerfile-deb11;
-		TAG=${TAG:-"deb11"};
+		OPTION_IMAGE=$1;
 	;;
 	2 )
-		IMAGE_NAME="$IMAGE_BASENAME:debian-buster-$ARCH";
-		DOCKERFILE=Dockerfile-deb10;
-		TAG=${TAG:-"deb10"};
-	;;
-	3 )
-		IMAGE_NAME="$IMAGE_BASENAME:centos-7-$ARCH";
-		DOCKERFILE=Dockerfile-centos7;
-		TAG=${TAG:-"centos7"};
+		OPTION_IMAGE=$1;
+		OPTION_COMMAND=$2;
 	;;
 	* )
 		exit 1;
 	;;
 esac
 
-IMAGES=$(docker image ls -q);		# Check image
-if [ -z $IMAGES ] || [ -v IMAGE_REBUILD ];
+# Ask image option if it is not provided
+if ! [ -v OPTION_IMAGE ];
 then
-	docker build --tag $IMAGE_NAME --file docker/$DOCKERFILE docker
+	echo "Available images:
+	[1] Debian 11 (reference)
+	[2] Debian 10 (legacy)
+	[3] CentOS 7  (legacy)
+	"
+	read -p "Select image. Enter number of an option: " OPTION_IMAGE;
+fi
+
+
+if ! [ -v OPTION_COMMAND ];
+then
+	echo "Available commands:
+	[1] (re)build image
+	[2] test run
+	[3] build binary
+	[4] push image
+	"
+	read -p "Select command. Enter number of an option: " OPTION_COMMAND;
+fi
+
+# Resolve option
+IMAGE_BASENAME="mixeme/columbo";
+case $OPTION_IMAGE in
+	1 | deb11 )
+		IMAGE_NAME="$IMAGE_BASENAME:debian-bullseye-$ARCH";
+		DOCKERFILE=Dockerfile-deb11;
+		TAG=${TAG:-"deb11"};
+	;;
+	2 | deb10 )
+		IMAGE_NAME="$IMAGE_BASENAME:debian-buster-$ARCH";
+		DOCKERFILE=Dockerfile-deb10;
+		TAG=${TAG:-"deb10"};
+	;;
+	3 | centos7 )
+		IMAGE_NAME="$IMAGE_BASENAME:centos-7-$ARCH";
+		DOCKERFILE=Dockerfile-centos7;
+		TAG=${TAG:-"centos7"};
+	;;
+	* )
+		echo "Unknown option. Exit";
+		exit 1;
+	;;
+esac
+
+case $OPTION_COMMAND in
+	1 | image )
+		IMAGE_REBUILD=1;
+	;;
+	2 | test )
+		TEST=1;
+	;;
+	3 | binary )
+		BUILD=1;
+	;;
+	4 | push )
+		IMAGE_REBUILD=1;
+		PUSH=1;
+	;;
+	* )
+		exit 2;
+esac
+
+echo "Docker image: $IMAGE_NAME";
+echo "Tag for binary build: $TAG";
+[ -v IMAGE_REBUILD ] && echo "Request to (re)build Docker image";
+[ -v TEST ] && echo "Request to make a test run";
+[ -v BUILD ] && echo "Request to build binary";
+[ -v PUSH ] && echo "Request to push Docker image";
+
+# Build Docker image
+IMAGES=$(docker image ls -q $IMAGE_NAME);		# Check image
+([ -z "$IMAGES" ] && echo "  No image found") || echo "  Found image(s): $IMAGES";
+if [ -z "$IMAGES" ] || [ -v IMAGE_REBUILD ];
+then
+	echo "+ (Re)build Docker image: $IMAGE_NAME";
+	docker build --tag $IMAGE_NAME --file scripts/docker/$DOCKERFILE scripts/docker
 fi
 
 if [ -v TEST ];
 then
+	echo "Test run...";
 	docker run \
 			-it --rm \
 			--name columbo-env \
@@ -63,6 +126,7 @@ fi
 
 if [ -v BUILD ];
 then
+	echo "Build binary..";
 	docker run \
 			--user $(id -u):$(id -g) \
 			-it --rm \
@@ -74,5 +138,12 @@ then
 			$IMAGE_NAME
 fi
 
+if [ -v PUSH ];
+then
+	echo "+ Push image...";
+	docker push $IMAGE_NAME;
+fi
+
+echo "All done!";
 exit 0;
 
