f706f4ee4aeb94bfe670501835048fa7edf20304
mixeme <mix.public@ya.ru>
2024-02-03 15:20:47 +0300

    Move types definition

--- core/tree.py
+++ core/tree.py
@@ -7,17 +7,7 @@ from PyQt5.QtGui import QStandardItemModel
 
 from core import nodes, file
 from core.nodes import PathArray
-
-
-class TreeType(Enum):
-    UNIFIED = 0
-    BY_DATE = 1
-
-
-class OperationType(Enum):
-    FILE_TREE = 0
-    FILTERED_TREE = 1
-    EMPTY_DIRS = 2
+from core.types import TreeType, OperationType
 
 
 class FileSortFilterProxyModel(QtCore.QSortFilterProxyModel):
