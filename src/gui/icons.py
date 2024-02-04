d9b6ee1e49fa051a95c66eb3ea66832096ebc339
mixeme <mix.public@ya.ru>
2024-02-04 11:28:56 +0300

    Remove constructor

--- src/gui/icons.py
+++ src/gui/icons.py
@@ -7,7 +7,6 @@ class IconsLoader:
     singleton = None
 
     def __init__(self, project_home) -> None:
-        super().__init__()
         self.folder = QIcon.fromTheme("folder", QIcon(path.join(project_home, "icons/folder.png")))
         self.file = QIcon.fromTheme("text-x-generic", QIcon(path.join(project_home, "icons/file.png")))
         IconsLoader.singleton = self
