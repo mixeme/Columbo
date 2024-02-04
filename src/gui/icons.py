04f40ee078c322b808d1bd328fa60096d45a5f20
mixeme <mix.public@ya.ru>
2024-02-04 11:29:17 +0300

    Update paths to icons

--- src/gui/icons.py
+++ src/gui/icons.py
@@ -7,6 +7,6 @@ class IconsLoader:
     singleton = None
 
     def __init__(self, project_home) -> None:
-        self.folder = QIcon.fromTheme("folder", QIcon(path.join(project_home, "icons/folder.png")))
-        self.file = QIcon.fromTheme("text-x-generic", QIcon(path.join(project_home, "icons/file.png")))
+        self.folder = QIcon.fromTheme("folder", QIcon(path.join(project_home, "resources/icons/folder.png")))
+        self.file = QIcon.fromTheme("text-x-generic", QIcon(path.join(project_home, "resources/icons/file.png")))
         IconsLoader.singleton = self
