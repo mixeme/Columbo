6ba36a8430c9259a751b6dcf78be91b1ba3dd29e
Mikhail Yenuchenko <public@mixeme.ru>
2025-03-16 20:54:30 +0300

    Assign default values

--- src/gui/ui.py
+++ src/gui/ui.py
@@ -294,9 +294,13 @@ class HistoryUI(QtWidgets.QMainWindow):
             if self.from_checked() == TreeType.UNIFIED:
                 set_as_root = context_menu.addAction("Set as root")
                 context_menu.addSeparator()
+            else:
+                set_as_root = None
             if self.from_checked() == TreeType.BY_DATE:
                 set_as_sub_path = context_menu.addAction("Set as sub-path")
                 context_menu.addSeparator()
+            else:
+                set_as_sub_path = None
 
             expand = context_menu.addAction("Expand recursively")
 
