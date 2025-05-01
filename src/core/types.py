d85089160181a4d15aaa7ab5e4d46771a445a54b
Mikhail Yenuchenko <public@mixeme.ru>
2025-05-01 22:44:46 +0300

    Update types.py

--- src/core/types.py
+++ src/core/types.py
@@ -5,7 +5,6 @@ from PyQt5.QtGui import QStandardItem
 
 TreeNode = list[QStandardItem]      # Type alias for return values
 TreeRow = list[QModelIndex]         # Type alias for row entry
-PathArray = list[str]               # Type alias for split path string
 
 
 class TreeType(Enum):
@@ -17,5 +16,3 @@ class OperationType(Enum):
     FILE_TREE = 0
     FILTERED_TREE = 1
     EMPTY_DIRS = 2
-
-
