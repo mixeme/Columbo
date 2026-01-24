ebc73dff54d3de719f1bb3079d65076dbb863ec1
mixeme <mix.public@ya.ru>
2026-01-24 21:38:25 +0300

    Update filetree_builder.py

--- src/core/filetree_builder.py
+++ src/core/filetree_builder.py
@@ -1,69 +1,69 @@
 import os
 
 from PyQt5 import QtCore
-from PyQt5.QtCore import QRunnable, QObject, pyqtSignal
+from PyQt5.QtCore import QRunnable
 from PyQt5.QtGui import QStandardItemModel
 
-from core import file, node, snapshot
+from core import file, node, snapshot, filetree_loader
 from core.data_model import FileSortFilterProxyModel
+from core.pyqtmiscellaneous import Signals
 from core.types import TreeType, OperationType
 
 
-# Define signals for events
-class Signals(QObject):
-    build_finished = pyqtSignal(OperationType, QStandardItemModel)
-    clear_finished = pyqtSignal(OperationType)
-    progress = pyqtSignal(str)
-
-
-class FileTreeWorker(QRunnable):
+class FileTreeWBuilder(QRunnable):
     columns = ["Name", "Last modified", "Snapshot"]
     signals = Signals()
 
-    def __init__(self, root_path: str):
-        """
-
-        :param root_path: A path for history storage
-        """
+    def __init__(self, loader: filetree_loader.FileTreeLoader):
         super().__init__()
-        # Store input values
-        self.root_path = root_path
+        # Store file lists loader
+        self._loader = loader
 
         # Declare fields
-        self.checked_options = None
-        self.operation = None
-        self.root_node = None
-        self.sort_rows = None
-        self.validator = None
         self.data_model = None
         self.dirs = []
         self.files = []
+        self._direction = None
+        self._operation = None
+        self._validator = None
+        self._root_node = None
+        self._sort_rows = None
+        self._data_model = None
+
+    def set_options(self, direction, operation) -> None:
+        self._direction = direction
+        self._operation = operation
+
+    def set_validator(self, validator) -> None:
+        self._validator = validator
+
+    def get_abs(self, path: str) -> str:
+        return os.path.join(self._loader.get_root(), path)
 
     def init_root(self) -> None:
         # Create root row
-        root_row = node.create_folder_node(self.root_path)
+        root_row = node.create_folder_node(self._loader.get_root())
 
         # Create data model
         model = QStandardItemModel()
         model.invisibleRootItem().appendRow(root_row)
-        model.setHorizontalHeaderLabels(FileTreeWorker.columns)
+        model.setHorizontalHeaderLabels(FileTreeWBuilder.columns)
 
         # Create proxy data model for sorting customization
         proxy_model = FileSortFilterProxyModel()
         proxy_model.setSourceModel(model)
-        self.sort_rows = lambda: proxy_model.sort(0, QtCore.Qt.AscendingOrder)
-        self.data_model = proxy_model
+        self._sort_rows = lambda: proxy_model.sort(0, QtCore.Qt.AscendingOrder)
+        self._data_model = proxy_model
 
         # Store 1st column of the root row
-        self.root_node = root_row[0]
+        self._root_node = root_row[0]
 
     def list_dir(self):
-        if (len(self.dirs) == 0) and (len(self.files) == 0):
-            self.dirs, self.files = file.list_dir(self.root_path)
+        if self.loader.is_empty():
+            self.loader.load()
 
     def drop_lists(self) -> None:
-        self.dirs = []
-        self.files = []
+        self.loader.reset()
 
     def create_tree(self, routine):
         # Create tree root
@@ -76,19 +76,21 @@ class FileTreeWorker(QRunnable):
         routine()
 
     def tree_files(self):
-        timestamp_fun = snapshot.get_timestamp_fun(self.checked_options[0])
+        timestamp_fun = snapshot.get_timestamp_fun(self.direction[0])
+
+        self_files = self.loader.files
 
         # Split path into components
-        files_parts = [(path.split(os.sep), date) for path, date in self.files]
+        files_parts = [(path.split(os.sep), date) for path, date in self_files]
 
         # Normalize paths to "by date" format: append a timestamp as the first path component
-        if self.checked_options[0] == TreeType.UNIFIED:
+        if self.direction[0] == TreeType.UNIFIED:
             for path_parts, _ in files_parts:
                 path_parts.insert(0, timestamp_fun(path_parts))
 
         # Define starting component of a path
         # "by date" view starts with a timestamp component; "unified" view skips a timestamp component
-        if self.checked_options[1] == TreeType.BY_DATE:
+        if self.direction[1] == TreeType.BY_DATE:
             start_pos = 0
         else:
             start_pos = 1
@@ -105,17 +107,22 @@ class FileTreeWorker(QRunnable):
                 timestamp = timestamp_fun(file_parts)
 
                 # Add file node
-                node.add_file_node(current, self.checked_options, filename, last_modified, timestamp)
+                node.add_file_node(current, self.direction, filename, last_modified, timestamp)
 
     def tree_empty_dirs(self):
-        for d in self.dirs:
-            if file.is_empty_dir(d, self.files):
+        self_dirs = self.loader.dirs
+        self_files = self.loader.files
+
+        for d in self_dirs:
+            if file.is_empty_dir(d, self_files):
                 # Find corresponding node for the root
                 node.descend_by_path(self.root_node, d.split(os.sep))
 
     def clear_files(self):
+        self_files = self.loader.files
+
         # Split path into components
-        files_parts = [(path.split(os.sep), date) for path, date in self.files]
+        files_parts = [(path.split(os.sep), date) for path, date in self_files]
 
         for file_parts, _ in files_parts:
             if self.validator(file_parts):
@@ -130,9 +137,11 @@ class FileTreeWorker(QRunnable):
         self.signals.clear_finished.emit(self.operation)
 
     def clear_empty_dirs(self):
-        for d in self.dirs:
-            if file.is_empty_dir(d, self.files):
-                path = os.path.join(self.root_path, d)
+        dirs, files = self.loader.get_lists()
+
+        for d in dirs:
+            if file.is_empty_dir(d, files):
+                path = self.get_abs(d)
                 try:
                     os.removedirs(path)
                     self.signals.progress.emit("Deleted " + path)
@@ -143,6 +152,9 @@ class FileTreeWorker(QRunnable):
         self.signals.clear_finished.emit(self.operation)
 
     def run(self) -> None:
+        if self.loader.is_empty():
+            self.filetree_load_action()
+
         # Define routine function independence of operation
         routine_tree = None
         routine_clear = None
@@ -171,14 +183,3 @@ class FileTreeWorker(QRunnable):
         if routine_clear is not None:
             self.list_dir()     # List directory
             routine_clear()     # Create tree nodes
-
-
-# Class is used to prevent error
-# RuntimeError: wrapped C/C++ object of type FileTreeWorker has been deleted
-class WorkerWrapper(QRunnable):
-    def __init__(self, worker: FileTreeWorker) -> None:
-        super().__init__()
-        self.worker = worker
-
-    def run(self) -> None:
-        self.worker.run()
